0738a92025a0e97935b78af2daa61583ee5163b7
yalishizhude <yalishizhude@163.com>
2015-11-30 22:41:46 +0800

    edit

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -2,12 +2,16 @@ var app = angular.module('Vidzy', ['ngResource', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
 $routeProvider
     .when('/', {
-	    templateUrl: 'partials/home.html',
-	    controller: 'HomeCtrl'
-	})
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
+    })
     .when('/add-video', {
         templateUrl: 'partials/video-form.html',
         controller: 'AddVideoCtrl'
+    })
+    .when('/video/:id', {
+        templateUrl: 'partials/video-form.html',
+        controller: 'EditVideoCtrl'
     })
     .otherwise({
         redirectTo: '/'
@@ -29,3 +33,17 @@ function($scope, $resource, $location){
         });
     };
 }]);
+app.controller('EditVideoCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function($scope, $resource, $location, $routeParams){   
+        var Videos = $resource('/api/videos/:id', { id: '@_id' }, {
+            update: { method: 'PUT' }
+        });
+        Videos.get({ id: $routeParams.id }, function(video){
+            $scope.video = video;
+        });
+        $scope.save = function(){
+            Videos.update($scope.video, function(){
+                $location.path('/');
+            });
+        };
+    }]);
--- routes/videos.js
+++ routes/videos.js
@@ -21,4 +21,24 @@ router.post('/', function(req, res){
         res.json(video);
     });
 });
+router.get('/:id', function(req, res) {
+    var collection = db.get('videos');
+    collection.findOne({ _id: req.params.id }, function(err, video){
+        if (err) throw err;
+        res.json(video);
+    });
+});
+router.put('/:id', function(req, res){
+    var collection = db.get('videos');
+    collection.update({
+        _id: req.params.id
+    },
+    {
+        title: req.body.title,
+        description: req.body.description
+    }, function(err, video){
+        if (err) throw err;
+        res.json(video);
+    });
+});
 module.exports = router;
